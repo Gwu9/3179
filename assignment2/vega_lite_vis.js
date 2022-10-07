6fca9d4347f6cf86772d7aaa84272d700eee4c71
Gwu9 <113669804+Gwu9@users.noreply.github.com>
2022-10-07 12:57:59 +1100

    Update vega_lite_vis.js

--- assignment2/vega_lite_vis.js
+++ assignment2/vega_lite_vis.js
@@ -1,4 +1,6 @@
-var spec2 = "https://gwu9.github.io/3179/assignment2/james.vg.json";
-      vegaEmbed('#symbol_map', spec2).then(function(result) {
+var specVis1 = "https://gwu9.github.io/3179/assignment2/james.vg.json";
+var specVis2 = "https://gwu9.github.io/3179/assignment2/bubplot.vg.json";
+      vegaEmbed('#vis1', specVis1);
+            vegaEmbed('#vis1', specVis1).then(function(result) {
         // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
       }).catch(console.error);
