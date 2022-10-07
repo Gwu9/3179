ff7be480f97669472a8266fcd60a70bf8ce7d3b7
Gwu9 <113669804+Gwu9@users.noreply.github.com>
2022-10-07 12:09:35 +1100

    Update vega_lite_vis.js

--- assignment2/vega_lite_vis.js
+++ assignment2/vega_lite_vis.js
@@ -1,4 +1,4 @@
-var spec2 = "https://gwu9.github.io/3179/assignment2/codemap.vg.json";
+var spec2 = "https://gwu9.github.io/3179/assignment2/james.vg.json";
       vegaEmbed('#symbol_map', spec2).then(function(result) {
         // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
       }).catch(console.error);
