c7f4c8be5f284f19e29c363157b726b574040490
Gwu9 <113669804+Gwu9@users.noreply.github.com>
2022-10-07 15:17:44 +1100

    Add files via upload

--- assignment2/vega_lite_vis.js
+++ assignment2/vega_lite_vis.js
@@ -1,6 +1,4 @@
-var specVis1 = "https://gwu9.github.io/3179/assignment2/james.vg.json";
-var specVis2 = "https://gwu9.github.io/3179/assignment2/bubplot.vg.json";
-      vegaEmbed('#vis1', specVis1);
-            vegaEmbed('#vis1', specVis1).then(function(result) {
+var spec1 = "codemap.vg.json";
+      vegaEmbed('#symbol_map', spec1).then(function(result) {
         // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
       }).catch(console.error);
