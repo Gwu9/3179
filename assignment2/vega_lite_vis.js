21cec3076db14e43271c12aa4771aec363b2b250
Gwu9 <113669804+Gwu9@users.noreply.github.com>
2022-10-07 15:29:33 +1100

    Delete assignment2 directory

--- assignment2/vega_lite_vis.js
+++
@@ -1,4 +0,0 @@
-var spec1 = "codemap.vg.json";
-      vegaEmbed('#symbol_map', spec1).then(function(result) {
-        // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-      }).catch(console.error);
