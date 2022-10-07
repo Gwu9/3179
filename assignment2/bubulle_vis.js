5d05f3ac62f61aca7fac05a784f69bd4d1065dbc
Gwu9 <113669804+Gwu9@users.noreply.github.com>
2022-10-07 15:43:24 +1100

    Add files via upload

---
+++ assignment2/bubulle_vis.js
@@ -0,0 +1,4 @@
+var spec2 = "bubplot.vg.json";
+      vegaEmbed('#symbol_bubble plot', spec2).then(function(result) {
+        // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+      }).catch(console.error);
