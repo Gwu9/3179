6961e81b13f261e7572213e62900a1a7528bd3e6
Gwu9 <113669804+Gwu9@users.noreply.github.com>
2022-10-13 22:19:07 +1100

    Delete bubulle_vis.js

--- assignment2/bubulle_vis.js
+++
@@ -1,4 +0,0 @@
-var spec2 = "bubplot.vg.json";
-      vegaEmbed('#symbol_bubble plot', spec2).then(function(result) {
-        // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-      }).catch(console.error);
